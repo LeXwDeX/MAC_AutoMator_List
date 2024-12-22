d27f43a1367ce667b4bc0c59dc97e147b29fdc4e
lex <sunsan05@gmail.com>
2024-12-22 21:22:34 +0800

    去掉cp-r

--- 压缩文件.sh
+++ 压缩文件.sh
@@ -11,21 +11,6 @@ fi
 path="${1%/*}"
 cd "$path"
 
-# 创建临时目录
-tempdir="${path}/temp_$$"
-mkdir "$tempdir" || {
-    echo "错误：无法创建临时目录"
-    exit 1
-}
-
-# 移动所有选定的文件和目录到临时目录中
-for f in "$@"
-do
-    # 获取目标文件名
-    name="${f##*/}"
-    cp -r "$f" "$tempdir/"
-done
-
 # 设置压缩包的基础名称
 if [ $# -eq 1 ]; then
     # 如果只选择了一个文件/文件夹，使用其名称作为压缩包名称
@@ -45,15 +30,14 @@ while [ -f "${path}/${output_zip}" ]; do
     ((counter++))
 done
 
-# 压缩所有临时目录中的内容
-cd "$tempdir"
-if ! zip -r "${path}/${output_zip}" -X . -x "*.DS_Store"; then
-    cd "$path"
-    rm -rf "$tempdir"
+# 获取所有文件的相对路径
+files=()
+for f in "$@"; do
+    files+=("${f##*/}")
+done
+
+# 直接在原位置压缩文件
+if ! zip -r "${path}/${output_zip}" -X "${files[@]}" -x "*.DS_Store"; then
     echo "错误：压缩过程失败"
     exit 1
 fi
-
-# 返回起始目录并清理临时目录
-cd "$path"
-rm -rf "$tempdir"
